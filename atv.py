e9905894e3aff3e4d716a2264a9df89e93ce3027
Alysson <146789167+AlyssonAraujjoo@users.noreply.github.com>
2023-10-30 13:54:20 -0300

    Add files via upload

---
+++ atv.py
@@ -0,0 +1,7 @@
+import time #importar a biblioteca time para uso do intervalo de tempo
+for fogos in range(10 ,0 , -1): #utilizar o for para pecorrer e fazer o loop de sequencia de 10 até 0
+    print(fogos)
+    time.sleep(1)
+
+#fora do for adiciona a ultima interação que será os fogos
+print("POW POW 🎆🎆🎆🔥🔥")
